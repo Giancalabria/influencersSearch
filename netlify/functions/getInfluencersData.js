a8d5415a3cefe878e06ac233ad136db7a6e314bd
gian <gianluca@salesql.com>
2025-01-23 23:01:30 -0300

    more fixes

--- netlify/functions/getInfluencersData.js
+++ netlify/functions/getInfluencersData.js
@@ -6,15 +6,16 @@ const getInfluencersData = async (event) => {
   const question1 = `Find the top ${influencersAmount} health influencers in the USA who use Twitter, returning their names, Twitter photo URLs, follower counts, categories they specialize in, and their total number of tweets.`;
   const influencersData = await callChatGPT(question1);
 
-  let reliabilityData = [];
-  for (const influencer of influencersData) {
+  const reliabilityPromises = influencersData.map(async (influencer) => {
     const question2 = `Compare the information from ${influencer.name} against ${tweets} tweets published over time ${time} with ${sources} reliable sources, calculating a reliability percentage based on the matches.`;
     const influencerReliability = await callChatGPT(question2);
-    reliabilityData.push({
+    return {
       name: influencer.name,
       trustscore: influencerReliability.trustscore || 0,
-    });
-  }
+    };
+  });
+
+  const reliabilityData = await Promise.all(reliabilityPromises);
 
   const formattedInfluencersData = influencersData.map((influencer) => {
     const reliability = reliabilityData.find((r) => r.name === influencer.name);
